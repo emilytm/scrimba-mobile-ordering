d92397ac262a1e743d2c134f91427816bf4fa2ee
emilytm <emilytmcintyre@gmail.com>
2022-12-02 11:03:11 -0500

    Rendering menu list on page load

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { menuArray } from './menuData.js'
 
+document.addEventListener('DOMContentLoaded', renderMenu(menuArray))
+
 function renderMenu(menuArray) {
     let menuHtml = ""
     const menuList = document.getElementById('menu-list')
+    menuList.innerHTML = ""
+
     menuArray.forEach(function(menuItem) {
         console.log(menuItem.name)
         menuHtml += `
@@ -23,23 +27,4 @@ function renderMenu(menuArray) {
     menuList.innerHTML = menuHtml    
 }
 
-renderMenu(menuArray)
-
-
-
-/*
-
-                <div class="menu-item">
-                    <p class="item-emoji">🍕</p>
-                    <section class="item-details">
-                        <p class="item-name">Pizza</p>
-                        <p class="ingredients">pepperoni, mushroom, mozzarella</p>
-                        <p class="price">$14</p>
-                    </section>
-                    <section class="item-interactions">
-                        <image class="add-button" src="./images/plus.png">
-                    </section>
-                </div>
-                <hr>
-
-*/
+//renderMenu(menuArray)
